74a67c9bbe52b9bcf66d45cec67083f99a0eb126
DAviral-28 <cs23bt053@iitdh.ac.in>
2026-07-16 01:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.57%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    double rec(double x,double n)
-    {
-        if(n==1)
-        return x;
-        return x*rec(x,n-1);
-    }
     double myPow(double x, int n) {
         if(n<0)
-        x=1/x;
-        if(x==1 || n==0)
+        {
+            x=1/x;
+            if(n==INT_MIN)
+            n=INT_MAX-1;
+            else
+            n=abs(n);
+        }
+        if(n==0)
         return 1;
-        if(x==-1 && n%2==0)
-        return 1;
-        else if(x==-1 && n%2==1)
-        return -1;
-        if(n!=INT_MIN)
-        n=abs(n);
-        else
-        n=INT_MAX-1;
-        if(x>0 && x<1 && n>10000000)
-        return 0;
-        double p=rec(x,n);
-        return p;
+        double h=myPow(x,n/2);
+        if(n%2==0)
+        return h*h;
+        return x*h*h;
     }
 };
