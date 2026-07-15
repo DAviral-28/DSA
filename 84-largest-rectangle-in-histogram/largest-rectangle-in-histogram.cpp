553c9245c3de3371c273bd65e795359918e51824
DAviral-28 <cs23bt053@iitdh.ac.in>
2026-07-15 12:37:40 +0530

    Time: 43 ms (9.54%) | Memory: 92 MB (5.64%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    vector<int> findpse(vector<int>v)
+    {
+        int n=v.size();
+        vector<int>vv(n);
+        stack<int>s;
+        for(int i=0;i<n;i++)
+        {
+            if(s.empty())
+            {
+                vv[i]=-1;
+                s.push(i);
+                continue;        
+            }
+        if(v[i]>v[s.top()])
+        {
+            vv[i]=s.top();
+            s.push(i);
+        }
+        else
+        {
+            while(!s.empty() && v[i]<=v[s.top()])
+            s.pop();
+            if(s.empty())
+            vv[i]=-1;
+            else
+            vv[i]=s.top();
+            s.push(i);
+        }
+        }
+        return vv;
+    }
+    vector<int> findnse(vector<int>v)
+    {
+        int n=v.size();
+        vector<int>vv(n);
+        stack<int>s;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s.empty())
+            {
+                vv[i]=n;
+                s.push(i);            
+            }
+        if(v[i]>v[s.top()])
+        {
+            vv[i]=s.top();
+            s.push(i);
+        }
+        else
+        {
+            while(!s.empty() && v[i]<=v[s.top()])
+            s.pop();
+            if(s.empty())
+            vv[i]=n;
+            else
+            vv[i]=s.top();
+            s.push(i);
+        }
+        }
+        return vv;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        vector<int>nse=findnse(heights);
+        vector<int>pse=findpse(heights);
+        int m=0;
+        for(int i=0;i<n;i++)
+        {
+            int left=pse[i];
+            int right=nse[i];
+            int a=((right-left-1)*1LL*heights[i]);
+            m=max(m,a);
+        }
+        return m;
+    }
+};
