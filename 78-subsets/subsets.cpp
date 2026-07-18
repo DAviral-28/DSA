1c2e6d12dede2509cc65784cfd273fbd166189ed
DAviral-28 <cs23bt053@iitdh.ac.in>
2026-07-19 00:38:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (82.41%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    void ps(vector<int>&v,int i,vector<vector<int>>&vv,vector<int>&cur)
+    {
+        if(i==v.size())
+        {
+            vv.push_back(cur);
+            return;
+        }
+        cur.push_back(v[i]);
+        ps(v,i+1,vv,cur);
+        cur.pop_back();
+        ps(v,i+1,vv,cur);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>v;
-        for(int i=0;i<(1<<n);i++)
-        {
-            vector<int>vv;
-            for(int j=0;j<n;j++)
-            {
-                if(((1<<j)&i)!=0)
-                vv.push_back(nums[j]);
-            }
-            v.push_back(vv);
-        }
+        vector<int>cur;
+        ps(nums,0,v,cur);
         return v;
     }
 };
