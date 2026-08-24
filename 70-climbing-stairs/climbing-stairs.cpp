513c84f51fef2883112c779213e34bd4176cdf26
DAviral-28 <cs23bt053@iitdh.ac.in>
2026-08-24 13:20:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.99%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n<=1)
+    int cs(vector<int>&dp,int i)
+    {
+        if(i<=1)
         return 1;
-        vector<int>dp(n+1);
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<=n;i++)
-        dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        if(dp[i]!=-1)
+        return dp[i];
+        dp[i]=cs(dp,i-1)+cs(dp,i-2);
+        return dp[i];
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return cs(dp,n);
     }
 };
